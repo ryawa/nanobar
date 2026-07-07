b47d07ca6d0879884d65090eb79f5d296ea87065
ryawa <22625346+ryawa@users.noreply.github.com>
2026-07-07 16:14:15 -0400

    Initial Commit

---
+++ nanobar/nanobarApp.swift
@@ -0,0 +1,17 @@
+//
+//  nanobarApp.swift
+//  nanobar
+//
+//  Created by Ryan Wang on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct nanobarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
